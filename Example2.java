0a3fd3d584581c900c9181f3f32ce28f9fdb44b6
Satyabrata2000 <satyabrata2000@gmail.com>
2022-12-11 19:48:51 -0600

    Exercise5

---
+++ Example2.java
@@ -0,0 +1,11 @@
+package Exercise5;
+
+//Convert String to int
+public class Example2 {
+
+    public static void main(String[] args) {
+        String s = "100";
+        int i = Integer.parseInt(s);
+        System.out.println(i);
+    }
+}
